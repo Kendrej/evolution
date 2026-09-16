c5bfa1bda45a3e828acf4d8c02ec12005a3922e1
Kendrej <jendrekaugustyniak@gmail.com>
2026-09-16 21:27:09 +0200

    DVD style animation

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,35 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Ewolucja")]
 async fn main() {
-    let mut x = 0.0;
-    let mut y = 0.0;
-    let mut i = 1.0;
+    let mut x = 10.0;
+    let mut y = 10.0;
+    let mut i = 1;
+    let mut j = 1;
 
     loop{
-        if x<screen_width()-5.0 {
-            x += 1.0;
+        if x >= screen_width() - 60.0 {
+            i = -1;
         }
-        if y<screen_height()-5.0 {
-            y += 1.0;
+        else if x <= 0.0 {
+            i = 1;
         }
+        if y >= screen_height() - 40.0 {
+            j = -1;
+        }
+        else if y <= 0.0 {
+            j = 1;
+        }
+        
+        
+        x += 1.0 * i as f32;
+        y += 1.0 * j as f32;
+
+
+
+
         clear_background(BLACK);
-        draw_circle(x, y, 15.0, RED);
+        draw_rectangle(x, y, 60.0, 40.0, BLUE);
+        //draw_circle(x, y, 15.0, RED);
         next_frame().await
     }
 }
