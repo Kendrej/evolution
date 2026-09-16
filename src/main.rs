5d66d2d5feb56207a3aa53d76cb8d9c66afb9ae8
Kendrej <jendrekaugustyniak@gmail.com>
2026-09-16 21:06:26 +0200

    window setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Ewolucja")]
+async fn main() {
+    let mut x = 0.0;
+    let mut y = 0.0;
+    let mut i = 1.0;
+
+    loop{
+        if x<screen_width()-5.0 {
+            x += 1.0;
+        }
+        if y<screen_height()-5.0 {
+            y += 1.0;
+        }
+        clear_background(BLACK);
+        draw_circle(x, y, 15.0, RED);
+        next_frame().await
+    }
 }
