c926976a315c6110d0023d499c9137f4fbc3e9e2
Kendrej <jendrekaugustyniak@gmail.com>
2026-09-19 12:35:30 +0200

    feat: vec of agents

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,26 @@ use agent::Agent;
 
 #[macroquad::main("Ewolucja")]
 async fn main() {
-    
+
     let mut agent =  Agent::new_with_position(10.0, 10.0, 5.0);
     let mut agent1 = Agent::new_with_position(100.0, 50.0, 7.5);
     let mut agent2 = Agent::new_with_position(20.0, 200.0, 10.0);
 
+    let mut agents = vec![agent, agent1, agent2];
+
+    
+    
+
+
     loop{
         
         clear_background(BLACK);
-        agent.update(screen_height(), screen_width());
-        agent.draw();
-        agent1.update(screen_height(), screen_width());
-        agent1.draw();
-        agent2.update(screen_height(), screen_width());
-        agent2.draw();
+
+        for a in &mut agents{
+            a.update(screen_height(), screen_width());
+            a.draw();
+        }
+
         next_frame().await
     }
 }
